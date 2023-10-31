27a5fb73075035ee4157fd8aed62b74cdae477c5
hepona <hepona10@gmail.com>
2023-10-31 22:30:34 +0100

    4 5

--- 2-main.c
+++ 2-main.c
@@ -12,12 +12,14 @@ int main(void)
     binary_tree_t *root;
 
     root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
+    root->left = binary_tree_insert_right(root, 666);
+
+    root->right = binary_tree_insert_right(root, 666);
+
     binary_tree_print(root);
     printf("\n");
     binary_tree_insert_right(root->left, 54);
-    binary_tree_insert_right(NULL, -5);
+    binary_tree_insert_right(root, 128);
     binary_tree_print(root);
     return (0);
 }
