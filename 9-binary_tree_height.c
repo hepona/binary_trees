ca549a212c16b61dfbe73188bc8e54973810df12
hepona <hepona10@gmail.com>
2023-11-01 23:22:22 +0100

    11

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,14 +6,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t h = 0;
+size_t h = 0;
 
-    if (tree == NULL)
-        return (0);
-
-    if (tree->right != NULL)
-        h++;
-    if (tree->left != NULL)
-        h++;
-    return (h);
+if (tree == NULL)
+return (0);
+if (tree->right != NULL)
+h++;
+if (tree->left != NULL)
+h++;
+return (h);
 }
