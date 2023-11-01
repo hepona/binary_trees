2272524d548151d614ad67c6ed0088a425702449
hepona <hepona10@gmail.com>
2023-11-01 14:18:19 +0100

    6 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -9,14 +9,8 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 if (tree == NULL || func == NULL)
 return;
-if (tree->left != NULL || tree->right != NULL)
-{
-func(tree->left->left->n);
-func(tree->left->n);
-func(tree->left->right->n);
+
+binary_tree_inorder(tree->left, func);
 func(tree->n);
-func(tree->right->left->n);
-func(tree->right->n);
-func(tree->right->right->n);
-}
+binary_tree_inorder(tree->right, func);
 }
