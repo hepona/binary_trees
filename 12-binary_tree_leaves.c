d0a4d76b36391f2c2a62cbb4204db6e532c963f1
hepona <hepona10@gmail.com>
2023-11-02 09:07:14 +0100

    12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leave - function that counts the leaves in a binary tree
+ * binary_tree_leaveS - function that counts the leaves in a binary tree
  * @tree: pointer to the root node of the tree to count the number of leaves
  * Return: number of leaves, 0 otherwise
  */
